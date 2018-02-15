bedf11a2e8326a16e5cc6ddb806f14cc7422c7aa
Mark Villudo <mark.villudo@synergy88digital.com>
2018-02-15 10:39:59 +0800

    include controller and calculator service provider

---
+++ devmark/calculator/src/CalculatorServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Devmark\Calculator;
+
+use Illuminate\Support\ServiceProvider;
+
+class CalculatorServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+        include __DIR__.'/routes.php';
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // register our controller
+        $this->app->make('Devmark\Calculator\CalculatorController');
+    }
+}
